8d0b82d0a86b190586c3491ce38c79a16a15f3ee
Andy Sigler <andrewsigler1@gmail.com>
2019-05-28 14:34:22 -0400

    vocoder fully functional and ready to record

--- experiments/2/js/main.js
+++ experiments/2/js/main.js
@@ -1,17 +1,16 @@
-
-
-function addAudioOnOffEvents(button, _onEvent, _offEvent) {
+function buttonOnOffEvents(button, onCallback, offCallback) {
+    console.log('Create On/Off Button');
     var onString = "Audio is On";
     var offString = "Audio is Off";
     var isPoweredOn = false;
     var context = undefined;
-    var onEvent = function () {
-        _onEvent();
+    var buttonOnEvent = function () {
+        onCallback();
         console.log(onString);
         button.innerHTML = onString;
     }
-    var offEvent = function () {
-        _offEvent();
+    var buttonOffEvent = function () {
+        offCallback();
         console.log(offString);
         button.innerHTML = offString;
     }
@@ -20,47 +19,60 @@ function addAudioOnOffEvents(button, _onEvent, _offEvent) {
             isPoweredOn = true;
             if (context === undefined) {
                 context = new AudioContext();
-                context.resume().then(onEvent);
+                context.resume().then(buttonOnEvent);
             }
             else {
-                onEvent();
+                buttonOnEvent();
             }
         }
         else {
             isPoweredOn = false;
-            offEvent();
+            buttonOffEvent();
         }
     });
+    buttonOffEvent();
 }
 
 function setupAudio() {
     var modulator = new Tone.Player();
-    modulator.load("http://localhost:8000/experiments/2/sample.mp3");
-    var noise = new Tone.Noise('white');
-    // source, modulator, output, numFilters, minFreq, maxFreq
-    var vocoder = new Vocoder(
-        noise, modulator, Tone.Master, 50, 50, 8000);
+    var carrier = new Tone.Noise('white');
+    var vocoder = new Vocoder(carrier, modulator, Tone.Master, {
+        gain: 0,
+        updateInterval: 1 / 64,     // seconds (0.015625)
+        filterRolloff: -48,         // -12db, -24db, or -48db
+        carrierQ: 10,               // must be >= 1
+        modulatorQ: 10,             // must be >= 1
+        maxBands: 32,
+        maxFreq: 15000,
+        minDecibels: -100,
+    });
+    var button = document.getElementById('button');
     var allOn = function() {
-        noise.start();
+        carrier.start();
         modulator.start();
+        vocoder.start(true);
     }
     var allOff = function() {
-        noise.stop();
+        carrier.stop();
         modulator.stop();
+        console.log(vocoder.stop());
     }
-    var button = document.getElementById('button');
-    addAudioOnOffEvents(button, allOn, allOff);
+    console.log('loading audio file...');
+    modulator.load(
+        "http://localhost:8000/experiments/2/sample.mp3",
+        () => {buttonOnOffEvents(button, allOn, allOff);}
+    );
     return vocoder;
 }
 
 var vocoder;
 
-function setup() {
+window.addEventListener('load', (event) => {
     vocoder = setupAudio();
-    vocoder.setGain(10);
-    console.log('here');
-}
+    vocoder.setGain(20);
+});
 
-function draw() {
-    vocoder.update();
+function q(newQ) {
+    vocoder.setCarrierQ(newQ);
+    vocoder.setModulatorQ(newQ);
 }
--- experiments/2/js/vocoder.js
+++ experiments/2/js/vocoder.js
@@ -1,57 +1,62 @@
-const filterBandRolloff = -48;  // -12, -24, or -48
-const filterBandQ = 10; // >=1
-
 class FilterBand {
-  constructor(input, output, numFilters, minFreq, maxFreq) {
-    this.length = numFilters;
-    this.filters = [];
-    this.volumes = [];
-    this.meters = [];
-    this.input = input;
-    this.output = output;
-    this.generateNodes(minFreq, maxFreq);
-    this.connectNodes();
-  }
-  generateNodes(minFreq, maxFreq) {
-    const fStep = (maxFreq - minFreq) / (this.length / 1);
-    console.log('Frequency Spread:', fStep);
+    constructor(input, output, frequencies, options) {
+        this.frequencies = frequencies.slice();
+        this.length = this.frequencies.length;
+        this.filters = [];
+        this.volumes = [];
+        this.outputVolume = undefined;
+        this.meters = [];
+        this.input = input;
+        this.output = output;
+        this.generateNodes(options);
+        this.connectNodes(options);
+    }
+  generateNodes(options) {
+    if (this.output) {
+        this.outputVolume = new Tone.Volume(0);
+    }
     for (var i=0;i<this.length;i++) {
         this.filters[i] = new Tone.Filter(
-          minFreq + (fStep * i),
-          'bandpass',
-          filterBandRolloff
+            this.frequencies[i],
+            'bandpass',
+            options.filterRolloff
         );
-        this.filters[i].Q.value = filterBandQ;
         if (this.output) {
-          this.volumes[i] = new Tone.Volume(0);
-          this.volumes[i].volume.value = 0;
+            this.volumes[i] = new Tone.Volume(0);
+            this.volumes[i].volume.value = 0;
         }
         else {
-          this.meters[i] = new Tone.Meter();
+            this.meters[i] = new Tone.Meter();
         }
     }
   }
-  connectNodes() {
+  connectNodes(options) {
+    if(this.output) {
+        this.outputVolume.connect(this.output);
+    }
     for (var i=0;i<this.length;i++) {
-      this.input.connect(this.filters[i]);
-      if (this.output) {
-        this.filters[i].connect(this.volumes[i]);
-        this.volumes[i].connect(this.output);
-      }
-      else {
-        this.filters[i].connect(this.meters[i]);
-      }
+        this.input.connect(this.filters[i]);
+        if (this.output) {
+            this.filters[i].connect(this.volumes[i]);
+            this.volumes[i].connect(this.outputVolume);
+        }
+        else {
+            this.filters[i].connect(this.meters[i]);
+        }
     }
   }
-  setBands(newSpectrum, gain) {
-    for (var i=0;i<this.length;i++) {
-      this.volumes[i].volume.value = newSpectrum[i] + gain;
+  setBands(newSpectrum) {
+    for (var i=0, len=this.length; i<len; i++) {
+        this.volumes[i].volume.value = newSpectrum[i];
     }
   }
-  getBands(newSpectrum) {
+  getBands(minDecibels) {
+    if (minDecibels === undefined) {
+        minDecibels = -100;
+    }
     var retVal = [];
-    for (var i=0;i<this.length;i++) {
-      retVal[i] = this.meters[i].getLevel();
+    for (var i=0, len=this.length; i<len; i++) {
+        retVal[i] = Math.max(this.meters[i].getLevel(), minDecibels);
     }
     return retVal;
   }
@@ -59,20 +64,131 @@ class FilterBand {
 
 
 class Vocoder {
-  constructor(source, modulator, output, numFilters, minFreq, maxFreq) {
-    this.sourceFilterBand = new FilterBand(
-        source, output, numFilters, minFreq, maxFreq);
-    this.modulatorFilterBand = new FilterBand(
-        modulator, undefined, numFilters, minFreq, maxFreq);
-    this.gain = 0.0;
-  }
-  update() {
-    this.sourceFilterBand.setBands(
-        this.modulatorFilterBand.getBands(),
-        this.gain
-    );
-  }
-  setGain(decibels) {
-    this.gain = decibels;
-  }
+    constructor(source, modulator, output, options) {
+        this.vocoderSetDefaultOptions(options);
+        this.frequencies = this.generateFrequencies();
+        this.length = this.frequencies.length;
+        this.carrier = new FilterBand(
+            source, output, this.frequencies, this.options);
+        this.modulator = new FilterBand(
+            modulator, undefined, this.frequencies, this.options);
+        this.isRecording = false;
+        this.recordBuffer = [];
+        this.setCarrierQ();
+        this.setModulatorQ();
+        this.setGain();
+    }
+    update() {
+        const spectrum = this.modulator.getBands(this.options.minDecibels);
+        this.carrier.setBands(spectrum);
+        return spectrum;
+    }
+    stop() {
+        if (this.eventID >= 0) {
+            Tone.Transport.stop();
+            Tone.Transport.clear(this.eventID);
+            this.eventID = undefined;
+            var retVal = this.recordBuffer.slice();
+            this.recordBuffer = [];
+            return retVal;
+        }
+    }
+    start(isRecording) {
+        var v = this;
+        var createUpdateScheduleEvent = function() {
+            if (isRecording) {
+                v.recordBuffer = [];
+                return function(time) {
+                    v.recordBuffer.push(v.update());
+                };
+            }
+            else {
+                return function(time) {
+                    v.update();
+                };
+            }
+        }
+        this.eventID = Tone.Transport.scheduleRepeat(
+            createUpdateScheduleEvent(),
+            this.options.updateInterval
+        );
+        Tone.Transport.start();
+    }
+    setGain(decibels) {
+        if (decibels === undefined) decibels = this.options.gain;
+        else this.options.gain = decibels;
+        this.carrier.outputVolume.volume.value = this.options.gain;
+    }
+    setCarrierQ(Q) {
+        if (Q === undefined) Q = this.options.carrierQ;
+        else this.options.carrierQ = Q;
+        for (var i=0, len=this.carrier.length;i<len;i++) {
+            this.carrier.filters[i].Q.value = this.options.carrierQ;
+        }
+    }
+    setModulatorQ(Q) {
+        if (Q === undefined) Q = this.options.modulatorQ;
+        else this.options.modulatorQ = Q;
+        for (var i=0, len=this.modulator.length;i<len;i++) {
+            this.modulator.filters[i].Q.value = this.options.modulatorQ;
+        }
+    }
+    getFrequencies() {
+        return this.modulator.filters.map(function(filter) {
+            return filter.frequency.value
+        });
+    }
+    generateFrequencies() {
+        var frequencies = [];
+        var count = 0;
+        while (frequencies.length == 0 || frequencies[frequencies.length - 1] < this.options.maxFreq) {
+            frequencies[count] = Math.pow(2, count / 4) * 10;
+            count += 1;
+        }
+        if (frequencies.length > this.options.maxBands) {
+            frequencies = frequencies.slice(
+                frequencies.length - this.options.maxBands);
+        }
+        return frequencies;
+    }
+    vocoderSetDefaultOptions(options) {
+        const vocoderDefaultOptions = {
+            gain: 0,
+            updateInterval: 1 / 64,     // seconds (0.015625)
+            filterRolloff: -48,         // -12db, -24db, or -48db
+            carrierQ: 10,                 // must be >= 1
+            modulatorQ: 10,                 // must be >= 1
+            maxBands: 32,
+            maxFreq: 15000,
+            minDecibels: -100,
+        };
+        if (options === undefined) {
+            options = vocoderDefaultOptions;
+        }
+        if (!options.gain) {
+            options.gain = vocoderDefaultOptions.gain;
+        }
+        if (!options.maxFreq) {
+            options.maxFreq = vocoderDefaultOptions.maxFreq;
+        }
+        if (!options.maxBands) {
+            options.maxBands = vocoderDefaultOptions.maxBands;
+        }
+        if (!options.updateInterval) {
+            options.updateInterval = vocoderDefaultOptions.updateInterval;
+        }
+        if (!options.filterRolloff) {
+            options.filterRolloff = vocoderDefaultOptions.filterRolloff;
+        }
+        if (!options.carrierQ) {
+            options.carrierQ = vocoderDefaultOptions.carrierQ;
+        }
+        if (!options.modulatorQ) {
+            options.modulatorQ = vocoderDefaultOptions.modulatorQ;
+        }
+        if (!options.minDecibels) {
+            options.minDecibels = vocoderDefaultOptions.minDecibels;
+        }
+        this.options = options;
+    }
 }
